796fd1a775db96ab402735a84b47911278c29e81
estevonac <nachoestevo@gmail.com>
2020-09-30 18:22:03 -0300

    Added Evento

--- TP TraceIt/src/ABM/Ciudadano.java
+++ TP TraceIt/src/ABM/Ciudadano.java
@@ -1,17 +1,26 @@
 package ABM;
 
-    public class Ciudadano extends Usuario {
+import Eventos.Evento;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class Ciudadano extends Usuario {
 
         private boolean habilitado;
-
+        int solicitudesCounter;
         int numeroTelefono;
         String cuil;
+        ArrayList<Evento> sintomas;
 
         public Ciudadano(String cuil, int numeroTelefono) {
             super(cuil);
             this.numeroTelefono = numeroTelefono;
             tipoUsuario = "ciudadano";
             habilitado = true;
+            solicitudesCounter = 0;
+            sintomas = new ArrayList<>();
         }
 
 
@@ -33,5 +42,42 @@ package ABM;
         public boolean getHabilitado() {
             return habilitado;
         }
+
+
+    public boolean tuvoContacto(String cuil, Date fechaDesde, Date fechaHasta){ // Le tenemos que mandar un mensaje al otro ciudadano para confirmar.
+                 /*if (){                                                         // Si la respuesta es no. Contador ++. if contador = 5 usuario bloqeuado
+
+
+                 }
+                 else{
+                     solicitudesCounter++;
+                     return false;
+                 }*/
+
+
+            return true;
+    }
+    public boolean tuvoContacto(int numeroTelefono, Date fechaDesde, Date fechaHasta){
+
+        /*if (){
+
+
+        }
+        else{
+            solicitudesCounter++;
+            return false;
+        }*/
+            return true;
     }
 
+    private  void presenciaSintomas(Evento unEvento){
+            sintomas.add(unEvento);
+
+    }
+    private void eliminarSintoma(Evento unEvento){
+            sintomas.remove(unEvento);
+
+
+
+    }
+}
